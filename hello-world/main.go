cf41175cf732de9649fb9a251c501942d253ce8d
Valerie Concepcion <vconcepcion@enova.com>
2018-06-19 22:48:00 -0500

    Show IP address

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"errors"
-//	"fmt"
-//	"io/ioutil"
-//	"net/http"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -22,26 +22,26 @@ var (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//resp, err := http.Get(DefaultHTTPGetAddress)
-	//if err != nil {
-	//	return events.APIGatewayProxyResponse{}, err
-	//}
+	resp, err := http.Get(DefaultHTTPGetAddress)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
 
-	//if resp.StatusCode != 200 {
-	//	return events.APIGatewayProxyResponse{}, ErrNon200Response
-	//}
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
 
-	//ip, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	return events.APIGatewayProxyResponse{}, err
-	//}
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
 
-	//if len(ip) == 0 {
-	//	return events.APIGatewayProxyResponse{}, ErrNoIP
-	//}
+	if len(ip) == 0 {
+		return events.APIGatewayProxyResponse{}, ErrNoIP
+	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello World!!", //fmt.Sprintf("Hello, %v", string(ip)),
+		Body:       fmt.Sprintf("Hello, %v", string(ip)),
 		StatusCode: 200,
 	}, nil
 }
